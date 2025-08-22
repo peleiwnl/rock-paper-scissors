6bb805e61d9e44f6fd6731b0616d53bb14275fbf
peleiwnl <moomayle@hotmail.co.uk>
2025-08-22 21:01:13 +0100

    Add RemoveEvent

--- src/server/game.server.ts
+++ src/server/game.server.ts
@@ -4,31 +4,42 @@ const startAppearEvent = ReplicatedStorage.FindFirstChild("StartAppearEvent") as
 
 const arenas = game.Workspace.WaitForChild("Arenas");
 
-function findSeatedPlayers(seat1: Seat, seat2: Seat) {
-	const checkSeats = () => {
-		print("Seat1 occupant:", seat1.Occupant);
-		print("Seat2 occupant:", seat2.Occupant);
+let currentPlayer1: Player | undefined;
+let currentPlayer2: Player | undefined;
 
-		const player1Humanoid = seat1.Occupant?.Parent;
-		const player2Humanoid = seat2.Occupant?.Parent;
+function checkSeats(seat1: Seat, seat2: Seat) {
+	const player1Humanoid = seat1.Occupant?.Parent;
+	const player2Humanoid = seat2.Occupant?.Parent;
 
-		if (player1Humanoid && player2Humanoid) {
-			const player1 = Players.GetPlayerFromCharacter(player1Humanoid);
-			const player2 = Players.GetPlayerFromCharacter(player2Humanoid);
+	if (player1Humanoid && player2Humanoid) {
+		const player1 = Players.GetPlayerFromCharacter(player1Humanoid);
+		const player2 = Players.GetPlayerFromCharacter(player2Humanoid);
 
-			if (player1 && player2) {
-				print("Both players seated");
-				//fire player1 and player2
-			}
+		if (player1 && player2) {
+			print("Both players seated");
+			startAppearEvent.FireClient(player1, true);
+			startAppearEvent.FireClient(player2, true);
+			currentPlayer1 = player1;
+			currentPlayer2 = player2;
+			return;
 		}
-	};
+	}
 
-	seat1.GetPropertyChangedSignal("Occupant").Connect(checkSeats);
-	seat2.GetPropertyChangedSignal("Occupant").Connect(checkSeats);
+	if (currentPlayer1 && currentPlayer2) {
+		print("One of the players has got up");
+		startAppearEvent.FireClient(currentPlayer1, false);
+		startAppearEvent.FireClient(currentPlayer2, false);
+		currentPlayer1 = undefined;
+		currentPlayer2 = undefined;
+	}
 }
 
 for (const arena of arenas.GetChildren()) {
 	const seat1 = arena.WaitForChild("Seat1") as Seat;
 	const seat2 = arena.WaitForChild("Seat2") as Seat;
-	findSeatedPlayers(seat1, seat2);
+	[seat1, seat2].forEach((seat) => {
+		seat.GetPropertyChangedSignal("Occupant").Connect(() => {
+			checkSeats(seat1, seat2);
+		});
+	});
 }
